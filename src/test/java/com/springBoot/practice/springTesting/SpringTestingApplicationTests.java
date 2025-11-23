7f9fb2ff9c81a7905377863e3fede442ff72363e
vijay <vijaynani505@gmail.com>
2025-11-23 16:54:27 +0530

    inital commit

---
+++ src/test/java/com/springBoot/practice/springTesting/SpringTestingApplicationTests.java
@@ -0,0 +1,10 @@
+package com.springBoot.practice.springTesting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringTestingApplicationTests {
+
+
+}
